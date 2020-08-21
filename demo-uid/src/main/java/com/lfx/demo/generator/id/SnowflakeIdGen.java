3e17c9bc875d734dac1a501bb4147e5690224304
linfuxin <Lin123456>
2020-08-21 17:20:26 +0800

    雪花算法，自增从随机数开始，避免最后一位总是从0开始

--- demo-uid/src/main/java/com/lfx/demo/generator/id/SnowflakeIdGen.java
+++ demo-uid/src/main/java/com/lfx/demo/generator/id/SnowflakeIdGen.java
@@ -1,37 +1,42 @@
 package com.lfx.demo.generator.id;
 
+import java.util.Random;
+
 public class SnowflakeIdGen {
 
     /**
      * 起始的时间戳: 2020-08-21 00:00:00
      */
-    private final static long START_TIME = 1597939200000L;
+    private static final  long START_TIME = 1597939200000L;
 
     /**
      * 每一部分占用的位数
      */
-    private final static long SEQUENCE_BIT = 12; //序列号占用的位数
-    private final static long MACHINE_BIT = 5;   //机器标识占用的位数
-    private final static long DATACENTER_BIT = 5;//数据中心占用的位数
+    private static final  long SEQUENCE_BIT = 12; //序列号占用的位数
+    private static final  long MACHINE_BIT = 5;   //机器标识占用的位数
+    private static final  long DATACENTER_BIT = 5;//数据中心占用的位数
 
     /**
      * 每一部分的最大值
      */
-    private final static long MAX_DATACENTER_NUM = ~(-1L << DATACENTER_BIT);
-    private final static long MAX_MACHINE_NUM = ~(-1L << MACHINE_BIT);
-    private final static long MAX_SEQUENCE = ~(-1L << SEQUENCE_BIT);
+    private static final  long MAX_DATACENTER_NUM = ~(-1L << DATACENTER_BIT);
+    private static final  long MAX_MACHINE_NUM = ~(-1L << MACHINE_BIT);
+    private static final  long MAX_SEQUENCE = ~(-1L << SEQUENCE_BIT);
 
     /**
      * 每一部分向左的位移
      */
-    private final static long MACHINE_LEFT = SEQUENCE_BIT;
-    private final static long DATACENTER_LEFT = SEQUENCE_BIT + MACHINE_BIT;
-    private final static long TIME_LEFT = DATACENTER_LEFT + DATACENTER_BIT;
+    private static final  long MACHINE_LEFT = SEQUENCE_BIT;
+    private static final  long DATACENTER_LEFT = SEQUENCE_BIT + MACHINE_BIT;
+    private static final  long TIME_LEFT = DATACENTER_LEFT + DATACENTER_BIT;
 
     private final long dataCenterId;  //数据中心
     private final long machineId;     //机器标识
     private long sequence = 0L; //序列号
     private long lastTime = -1L;//上一次时间戳
+
+    private static final Random RANDOM = new Random();
+
 
     public SnowflakeIdGen(long dataCenterId, long machineId) {
         if (dataCenterId > MAX_DATACENTER_NUM || dataCenterId < 0) {
@@ -55,11 +60,13 @@ public class SnowflakeIdGen {
             sequence = (sequence + 1) & MAX_SEQUENCE;
             //同一毫秒的序列数已经达到最大
             if (sequence == 0L) {
+                //seq 为0的时候表示是下一毫秒时间开始对seq做随机
+                sequence = RANDOM.nextInt(100);
                 currStmp = getNextMill();
             }
         } else {
-            //不同毫秒内，序列号置为0
-            sequence = 0L;
+            //不同毫秒内，序列号重新开始
+            sequence = RANDOM.nextInt(100);
         }
 
         lastTime = currStmp;
@@ -87,7 +94,7 @@ public class SnowflakeIdGen {
         System.out.println(t);
         System.out.println(Integer.toBinaryString(t));
         SnowflakeIdGen snowFlake = new SnowflakeIdGen(2, 3);
-        for (int i = 0; i < (1 << 2); i++) {
+        for (int i = 0; i < (1 << 4); i++) {
             System.out.println(snowFlake.nextId());
         }
 
