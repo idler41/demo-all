e5e2eeb1e590aae1e487fb689ba93dae45afad05
linfx <12345678>
2021-12-22 17:49:20 +0800

    添加mqtt模块

---
+++ demo-mqtt/src/main/java/com/lfx/demo/App.java
@@ -0,0 +1,9 @@
+package com.lfx.demo;
+
+/**
+ * Created on 2021-12-22 17:17:27
+ *
+ * @author linfuxin
+ */
+public class App {
+}
---
+++ demo-mqtt/src/main/java/com/lfx/demo/config/MqttConfig.java
@@ -0,0 +1,12 @@
+package com.lfx.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created on 2021-12-22 17:17:45
+ *
+ * @author linfuxin
+ */
+@Configuration
+public class MqttConfig {
+}
