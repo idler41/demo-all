19ec541a47d36289f8235f1e0552b7943b6dd8e2
linfuxin <idler41@163.com>
2023-02-22 17:33:35 +0800

    rocketmq native consumer 初步设计

--- demo-rocketmq/src/main/java/com/lfx/demo/rocketmq/consumer/NativeDispatchConsumer.java
+++ demo-rocketmq/src/main/java/com/lfx/demo/rocketmq/consumer/NativeDispatchConsumer.java
@@ -5,6 +5,9 @@ import com.lfx.demo.rocketmq.constant.AppConstant;
 import com.lfx.demo.rocketmq.enums.TopicEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
@@ -14,6 +17,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
@@ -21,6 +25,7 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -29,8 +34,9 @@ import java.util.stream.Collectors;
  */
 @Slf4j
 @Component
+@ConditionalOnMissingBean(DispatchConsumer.class)
 @RocketMQMessageListener(topic = "${rocketmq.consumer.topic:}", consumerGroup = "${rocketmq.consumer.group:}")
-public class DispatchConsumer implements RocketMQListener<MessageExt>, RocketMQPushConsumerLifecycleListener,
+public class NativeDispatchConsumer implements RocketMQListener<MessageExt>, RocketMQPushConsumerLifecycleListener, MessageListenerConcurrently,
         InitializingBean, ApplicationContextAware {
 
     @Value("${rocketmq.consumer.consumeThreadMax:20}")
@@ -47,29 +53,24 @@ public class DispatchConsumer implements RocketMQListener<MessageExt>, RocketMQP
 
     @Override
     public void onMessage(MessageExt messageExt) {
-        // 这里改为分布式id
-        MDC.put(AppConstant.TRACE_ID_KEY, String.valueOf(System.currentTimeMillis()));
-        if (log.isDebugEnabled()) {
-            log.info("received message,topic={},tags={},msgId={},times={}, body={}",
-                    messageExt.getTopic(), messageExt.getTags(), messageExt.getMsgId(), messageExt.getReconsumeTimes(), new String(messageExt.getBody()));
-        } else {
-            log.info("received message,topic={},tags={},msgId={},times={}", messageExt.getTopic(), messageExt.getTags(), messageExt.getMsgId(), messageExt.getReconsumeTimes());
-        }
-        TopicEnum topicEnum = TopicEnum.getInstance(messageExt.getTopic(), messageExt.getTags());
-        if (topicEnum == null) {
-            log.warn("no message handler match!body={}", new String(messageExt.getBody()));
-            return;
-        }
-        MessageHandler<? super Serializable> handler = handlerMap.get(topicEnum);
+        throw new UnsupportedOperationException();
+    }
 
-        Serializable content;
-        try {
-            content = objectMapper.readValue(messageExt.getBody(), handler.getDataType());
-        } catch (IOException e) {
-            // 在重试时间范围内，修复反序列化问题可防止消息丢失
-            throw new RuntimeException(e);
+    @Override
+    public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> messageExtList, ConsumeConcurrentlyContext context) {
+        ConsumeConcurrentlyStatus result = null;
+        for (MessageExt messageExt : messageExtList) {
+            try {
+                ConsumeConcurrentlyStatus consumeResult = doConsumeMessage(messageExt, context);
+                if (result == null || result == ConsumeConcurrentlyStatus.CONSUME_SUCCESS) {
+                    result = consumeResult;
+                }
+            } catch (Exception e) {
+                log.warn("consume message failed. reConsumeTimes:{}", messageExt.getReconsumeTimes(), e);
+                result = ConsumeConcurrentlyStatus.RECONSUME_LATER;
+            }
         }
-        handler.process(content);
+        return result != null ? result : ConsumeConcurrentlyStatus.RECONSUME_LATER;
     }
 
     @Override
@@ -77,6 +78,8 @@ public class DispatchConsumer implements RocketMQListener<MessageExt>, RocketMQP
         // 参数化改造
         consumer.setConsumeThreadMax(consumeThreadMax);
         consumer.setConsumeThreadMin(consumeThreadMin);
+        // 这里改为rocketmq原生监听器
+        consumer.setMessageListener(this);
         if (log.isDebugEnabled()) {
             log.debug("修改mq消费者线程数!");
         }
@@ -99,5 +102,25 @@ public class DispatchConsumer implements RocketMQListener<MessageExt>, RocketMQP
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
+    }
+
+    private ConsumeConcurrentlyStatus doConsumeMessage(MessageExt messageExt, ConsumeConcurrentlyContext context) throws IOException {
+        // 这里改为分布式id
+        MDC.put(AppConstant.TRACE_ID_KEY, String.valueOf(System.currentTimeMillis()));
+        if (log.isDebugEnabled()) {
+            log.info("received message,topic={},tags={},msgId={},times={}, body={}",
+                    messageExt.getTopic(), messageExt.getTags(), messageExt.getMsgId(), messageExt.getReconsumeTimes(), new String(messageExt.getBody()));
+        } else {
+            log.info("received message,topic={},tags={},msgId={},times={}", messageExt.getTopic(), messageExt.getTags(), messageExt.getMsgId(), messageExt.getReconsumeTimes());
+        }
+        TopicEnum topicEnum = TopicEnum.getInstance(messageExt.getTopic(), messageExt.getTags());
+        if (topicEnum == null) {
+            log.warn("no message handler match!body={}", new String(messageExt.getBody()));
+            // 本批次先消费的消费异常不要影响后消费的消息，尽量保证影响面小
+            return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+        }
+        MessageHandler<? super Serializable> handler = handlerMap.get(topicEnum);
+        handler.process(objectMapper.readValue(messageExt.getBody(), handler.getDataType()));
+        return null;
     }
 }
